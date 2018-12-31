51e1eb9203e2fe0e0f914f6ed45337d19ae8245a
Lucas Graybuck <lucasg@alleninstitute.org>
2018-12-31 13:12:37 -0800

    added a column for the nearest R color() name.

--- misc/anchor_colors_from_pdf.R
+++ misc/anchor_colors_from_pdf.R
@@ -377,6 +377,22 @@ anchor_colors$id <- str_pad(anchor_colors$id,
 anchor_colors <- anchor_colors %>%
   select(-page, -pcol, -coln)
 
+r_rgb <- col2rgb(colors(distinct = TRUE))
+colnames(r_rgb) <- colors(distinct = TRUE)
+
+nearest_r_color <- function(hexes) {
+  r_color_match <- map_chr(hexes,
+                           function(hex) {
+                             diffs <- apply(r_rgb, 2, function(x) {
+                               sum(abs(x - col2rgb(hex)))
+                             })
+                             colnames(r_rgb)[which(diffs == min(diffs))][1]
+                           })
+
+}
+
+anchor_colors$r_color <- nearest_r_color(anchor_colors$med_color)
+
 write.csv(anchor_colors,
           "inst/data/anchor_solid_colors.csv",
           row.names = F)
